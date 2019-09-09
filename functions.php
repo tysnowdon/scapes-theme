04c2a3377b749c88f68afaa2c60dd0447dc0e8b4
tysnowdon <tyjsnowdonsecure@gmail.com>
2019-09-09 13:59:59 +0100

    Bootsrap style/script pulled in, basic navbar in place

---
+++ functions.php
@@ -0,0 +1,43 @@
+<?php
+
+
+// /**
+//  * Custom navigation setup
+//  */
+// function scapes_custom_menu() {
+//     register_nav_menu('my-custom-menu',__( 'My Custom Menu' ));
+// }
+// add_action( 'init', 'scapes_custom_menu' );
+
+
+// //Replacement nav
+// wp_nav_menu( array( 
+//     'theme_location' => 'my-custom-menu', 
+//     'container_class' => 'custom-menu-class',
+//     'container_id' => 'custom-menu') ); 
+
+/**
+ * Register Styles and scripts
+ */
+//CSS
+wp_enqueue_style('prefix_bootstrap', get_stylesheet_directory_uri().'/css/bootstrap.min.css', array(), date("h:i:s"));
+
+//JS
+//Note that jQuery is automatically pulled in by WP
+wp_enqueue_script('prefix_bootstrap', get_stylesheet_directory_uri().'/js/bootstrap.min.js', array('jquery'), date("h:i:s"), true);
+
+
+
+
+
+
+
+
+include('navigation.php');
+
+
+
+
+
+
+
